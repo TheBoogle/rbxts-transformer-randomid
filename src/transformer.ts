8b6fa2c9a417683a611faf49ca379ca7548654a5
Boogle <theboogledoo@gmail.com>
2025-05-09 15:56:16 -0500

    erm

--- src/transformer.ts
+++ src/transformer.ts
@@ -8,9 +8,7 @@ export interface TransformerConfig {
 }
 
 /**
- * This is a utility object to pass around your dependencies.
- *
- * You can also use this object to store state, e.g prereqs.
+ * Utility context object to hold references.
  */
 export class TransformContext {
 	public factory: ts.NodeFactory;
@@ -24,94 +22,49 @@ export class TransformContext {
 	}
 
 	/**
-	 * Transforms the children of the specified node.
+	 * Recursively transforms the children of a node.
 	 */
 	transform<T extends ts.Node>(node: T): T {
 		return ts.visitEachChild(node, (node) => visitNode(this, node), this.context);
 	}
 }
 
-function visitImportDeclaration(context: TransformContext, node: ts.ImportDeclaration) {
-	const { factory } = context;
-
-	const path = node.moduleSpecifier;
-	const clause = node.importClause;
-	if (!clause) return node;
-	if (!ts.isStringLiteral(path)) return node;
-	if (path.text !== "@rbxts/services") return node;
-
-	const namedBindings = clause.namedBindings;
-	if (!namedBindings) return node;
-	if (!ts.isNamedImports(namedBindings)) return node;
-
-	return [
-		// We replace the import declaration instead of stripping it to prevent
-		// issues with isolated modules.
-		factory.updateImportDeclaration(
-			node,
-			undefined,
-			factory.createImportClause(false, undefined, factory.createNamedImports([])),
-			node.moduleSpecifier,
-			undefined,
-		),
-
-		// Creates a multi-variable statement as shown below.
-		//
-		// const Players = game.GetService("Players"),
-		//		Workspace = game.GetService("Workspace");
-		factory.createVariableStatement(
-			undefined,
-			factory.createVariableDeclarationList(
-				namedBindings.elements.map((specifier) => {
-					const serviceName = specifier.propertyName ? specifier.propertyName.text : specifier.name.text;
-					const variableName = specifier.name;
-
-					return factory.createVariableDeclaration(
-						variableName,
-						undefined,
-						undefined,
-						factory.createCallExpression(
-							factory.createPropertyAccessExpression(factory.createIdentifier("game"), "GetService"),
-							undefined,
-							[factory.createStringLiteral(serviceName)],
-						),
-					);
-				}),
-				ts.NodeFlags.Const,
-			),
-		),
-	];
-}
-
-function visitStatement(context: TransformContext, node: ts.Statement): ts.Statement | ts.Statement[] {
-	// This is used to transform statements.
-	// TypeScript allows you to return multiple statements here.
-
-	if (ts.isImportDeclaration(node)) {
-		// We have encountered an import declaration,
-		// so we should transform it using a separate function.
-
-		return visitImportDeclaration(context, node);
-	}
-
-	return context.transform(node);
-}
-
-function visitExpression(context: TransformContext, node: ts.Expression): ts.Expression {
-	// This can be used to transform expressions
-	// For example, a call expression for macros.
-
-	return context.transform(node);
-}
-
-function visitNode(context: TransformContext, node: ts.Node): ts.Node | ts.Node[] {
-	if (ts.isStatement(node)) {
-		return visitStatement(context, node);
-	} else if (ts.isExpression(node)) {
+/**
+ * Visit a node and determine if we want to transform it.
+ */
+function visitNode(context: TransformContext, node: ts.Node): ts.Node {
+	if (ts.isExpression(node)) {
 		return visitExpression(context, node);
 	}
 
-	// We encountered a node that we don't handle above,
-	// but we should keep iterating the AST in case we find something we want to transform.
 	return context.transform(node);
+}
+
+/**
+ * Transforms `$id()` calls into string literals containing UUIDs.
+ */
+function visitExpression(context: TransformContext, node: ts.Expression): ts.Expression {
+	if (ts.isCallExpression(node)) {
+		const expression = node.expression;
+
+		if (ts.isIdentifier(expression) && expression.text === "$id") {
+			const uuid = crypto.randomUUID(); // Requires Node 15.6+
+			return context.factory.createStringLiteral(uuid);
+		}
+	}
+
+	return context.transform(node);
+}
+
+/**
+ * Entry point for the transformer.
+ */
+export default function transformer(
+	program: ts.Program,
+	config: TransformerConfig,
+): ts.TransformerFactory<ts.SourceFile> {
+	return (context: ts.TransformationContext) => {
+		const transformContext = new TransformContext(program, context, config);
+		return (file: ts.SourceFile) => transformContext.transform(file);
+	};
 }
